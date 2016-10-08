2afe364cfd661ecf87d422d4f7c884523315ebd2
santikon <santikon_-apai@hotmail.com>
2016-10-08 15:51:35 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -8,3 +8,14 @@ S_states.each do | sub_array |
     end
 end
 end
+
+#5
+S_states = [["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]]
+ 
+S_states.each do | sub_array |
+    sub_array.each do |y|
+    if y[-1] == "a" or y[-1] == "e" or y[-1] == "i" or y[-1] == "o" or y[-1] == "u"
+        puts y
+    end
+end
+end
